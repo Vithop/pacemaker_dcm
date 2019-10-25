99d72283b956eacd25091452cbf6563b4b238128
Vithop <vithuransoccer1@gmail.com>
2019-10-25 13:54:13 -0400

    Added: SignUp page

---
+++ src/views/SignUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="signUp">
+    <h1>This is a sign up page</h1>
+    <input v-model="username" placeholder="User Name">
+    <p>User Name </p>
+    <input v-model="tempPassWord1" placeholder="User Name">
+    <p>Password </p>
+    <input v-model="tempPassWord2" placeholder="User Name">
+    <p>Retype Password </p>
+    <button v-on:click="doSignUp">Sign Up</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'signUp'
+}
+</script>
