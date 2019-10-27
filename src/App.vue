9f26085a8816c62e72cda83a9193e6af42105119
Vithop <vithuransoccer1@gmail.com>
2019-10-27 19:48:14 -0400

    Added: status bar into Navbar

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,7 @@
 		<q-layout view="hHh lpR fFf">
 			<SideBar v-if="this.$store.state.currentUser != ''" />
 			<q-page-container>
-				<div id="nav">
-					<div v-if="this.$store.state.currentUser == ''">
-						<q-tabs active-color="teal-14" class="text-blue-grey-7">
-							<q-route-tab label="Login" icon="ion-log-in" to="/" exact />
-							<q-route-tab label="Sign Up" icon="ion-person-add" to="/SignUp" exact />
-						</q-tabs>
-					</div>
-					<div v-else>should be logged in</div>
-				</div>
+        <NavBar/>
 				<router-view />
 			</q-page-container>
 		</q-layout>
@@ -20,13 +12,13 @@
 
 <script>
 import SideBar from "@/components/SideBar";
-import { QTabs, QRouteTab } from "quasar";
+import NavBar from "@/components/NavBar";
 
 export default {
+  name: "app",
 	components: {
-		SideBar,
-		QTabs,
-		QRouteTab
+    SideBar,
+    NavBar,
 	}
 };
 </script>
@@ -39,17 +31,7 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 }
-
-#welcomePage {
-	padding: 30px;
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
+p{
+	font-size: 15px
 }
 </style>
