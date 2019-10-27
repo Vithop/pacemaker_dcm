9e34c2a12d7870a59d976a1ba7f943185e352880
Vithop <vithuransoccer1@gmail.com>
2019-10-27 14:40:50 -0400

    Fixed: Navbar of Login

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,55 @@
 <template>
-  <div id="app">
-    <q-layout view="hHh lpR fFf">
-    
-      <SideBar v-if="this.$store.state.currentUser != ''"/>
-      <q-page-container>
-        <div id="nav">
-          <div id="welcomePage" v-if="this.$store.state.currentUser == ''">
-            <router-link to="/">Login</router-link> |
-            <router-link to="/SignUp">Sign Up</router-link>
-          </div>
-          <div v-else> should be logged in </div>
-        </div>
-        <router-view/>
-      </q-page-container>
-    </q-layout>
-  </div>
+	<div id="app">
+		<q-layout view="hHh lpR fFf">
+			<SideBar v-if="this.$store.state.currentUser != ''" />
+			<q-page-container>
+				<div id="nav">
+					<div v-if="this.$store.state.currentUser == ''">
+						<q-tabs active-color="teal-14" class="text-blue-grey-7">
+							<q-route-tab label="Login" icon="ion-log-in" to="/" exact />
+							<q-route-tab label="Sign Up" icon="ion-person-add" to="/SignUp" exact />
+						</q-tabs>
+					</div>
+					<div v-else>should be logged in</div>
+				</div>
+				<router-view />
+			</q-page-container>
+		</q-layout>
+	</div>
 </template>
 
 <script>
 import SideBar from "@/components/SideBar";
+import { QTabs, QRouteTab } from "quasar";
+
 export default {
-  components:{
-    SideBar
-  }
-
-
-}
+	components: {
+		SideBar,
+		QTabs,
+		QRouteTab
+	}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #welcomePage {
-  padding: 30px;
+	padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
