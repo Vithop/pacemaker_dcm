ac7d846701d069c9e2bbf42a4069a5c9fbad71df
Mathuran Sadagopan <mathuransada@gmail.com>
2019-10-27 15:50:56 -0400

    added pacing modes

---
+++ src/views/PacingModes.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <h2> pacing mode view </h2>
+  <q-select 
+    rounded 
+    outlined 
+    v-model="paceType" 
+    :options="paceOptions" 
+    label="Pacing Mode"
+  />
+</div>
+</template>
+<script>
+import {QSelect} from "quasar"
+
+export default {
+    name:'pacingmodes',
+    components: {
+      QSelect
+    },
+    data() {
+      return {
+        paceType: null,
+        paceOptions: [
+          'AOO', 'VOO', 'AII', 'VII'
+        ]
+      }
+    }
+}
+</script>
+
+<style scoped>
+</style>
