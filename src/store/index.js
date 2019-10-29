f683556aa71f69a11324a7fb697bb4b659639b0c
Vithop <vithuransoccer1@gmail.com>
2019-10-29 12:04:01 -0400

    Added: Password bcrypt

--- src/store/index.js
+++ src/store/index.js
@@ -1,60 +1,75 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import VuexPersist from 'vuex-persist'
+import Vue from "vue";
+import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
+import bcrypt from "bcrypt";
 
-
-Vue.use(Vuex)
+Vue.use(Vuex);
+const saltRounds = 10;
 
 const vuexPersist = new VuexPersist({
-  key: 'app',
+  key: "app",
   storage: window.localStorage
-})
+});
 
 export default new Vuex.Store({
   plugins: [vuexPersist.plugin],
   state: {
-    users:['admin'],
-    passwords:['password123'],
-    currentUser:''
+    users: ["admin"],
+    passwords: ["password123"],
+    currentUser: ""
   },
   mutations: {
-    signUp (state, payload){
-      state.users.push(payload.username)
-      state.passwords.push(payload.password)
+    signUp(state, payload) {
+      state.users.push(payload.username);
+      state.passwords.push(payload.password);
       state.currentUser = payload.username;
     },
-    setCurrentUsers(state,username){
-      state.currentUser = username
+    setCurrentUsers(state, username) {
+      state.currentUser = username;
     }
   },
   actions: {
-    signUp({commit}, payload){
-      // TODO: Add encrytion here and in login 
-      // TODO: Add local storage 
-      commit('signUp',{username : payload.username, password: payload.password})
+    signUp({ commit }, payload) {
+      // TODO: Add encrytion here and in login
+      bcrypt.hash(payload.password, saltRounds).then(hash => {
+        commit("signUp", {
+          username: payload.username,
+          password: hash
+        });
+      });
     },
-    login({commit, state}, payload){
-      return new Promise((resolve) =>{
-        console.log(state.users)
-        console.log(payload)
+    login({ commit, state }, payload) {
+      return new Promise(resolve => {
+        console.log(state.users);
+        console.log(payload);
         var userIndex = state.users.indexOf(payload.username);
-        console.log("userIndex", userIndex)
-        if(userIndex != -1){
-          if (state.passwords[userIndex] == payload.password ){
-            commit('setCurrentUsers', payload.username)
-            resolve("Login Succesfull!")
-          }else{
-            throw new Error("Password is Incorrect!")
+        console.log("userIndex", userIndex);
+        if (userIndex != -1) {
+          if(userIndex == 0){
+            if(state.passwords[userIndex] == payload.password){
+              commit("setCurrentUsers", payload.username);
+              resolve("Admin Has Logged In!")
+            }
           }
-        }else{
-          throw new Error("User doesn't exist!! Go Sign Up!")
+          bcrypt
+            .compare(payload.password, state.passwords[userIndex])
+            .then(res => {
+              if (res) {
+                commit("setCurrentUsers", payload.username);
+                resolve("Login Succesfull!");
+              } else {
+                console.log(res)
+                throw new Error("Password is Incorrect!");
+              }
+            });
+        } else {
+          throw new Error("User doesn't exist!! Go Sign Up!");
         }
-      })
+      });
     },
-    logout({commit}){
-      commit('setCurrentUsers','')
+    logout({ commit }) {
+      commit("setCurrentUsers", "");
     }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
